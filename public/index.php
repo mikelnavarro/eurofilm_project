cef4ee3649c1807bd4522dbfe0c749d581d0de07
mikelnaval2006@gmail.com <mikelnaval2006@gmail.com>
2026-05-20 18:24:19 +0200

    Variables de Entorno secretas, modificaciones a reformar

--- public/index.php
+++ public/index.php
@@ -4,7 +4,9 @@
 session_start();
 // Cargar el autoloader
 require_once __DIR__ . '/../vendor/autoload.php';
-
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
+$dotenv->load();
 // 3. Arrancar el Core
 // Como usas namespaces, instancia el Core desde su ubicación
 $app = new \Mikelnavarro\Eurofilm\Core\Core();
+
--- src/config/config.php
+++ src/config/config.php
@@ -2,22 +2,23 @@
 return [
     // Configuración de la Base de Datos
     'db' => [
-        'host'     => 'localhost',
-        'dbname'   => 'eurofilm_db',
-        'user'     => 'dweb',
-        'pass'     => '12345',
-        'charset'  => 'utf8mb4',
+        'host'     => $_ENV["DB_HOST"],
+        'dbname'   => $_ENV["DB_NAME"],
+        'user'     => $_ENV["DB_USER"] ?? 'root',
+        'pass'     => $_ENV["DB_PASS"] ?? '',
+        'charset'  => $_ENV["DB_CHARSET"] ?? 'utf8mb4'
     ],
     // Configuración de TMDB
     'tmdb' => [
-        'api_key' => 'af29f8a0d4727927172c7825408f2cda',
-        'token' => 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZjI5ZjhhMGQ0NzI3OTI3MTcyYzc4MjU0MDhmMmNkYSIsIm5iZiI6MTc3NjI4MDQyMC43NTIsInN1YiI6IjY5ZGZlMzY0MTY4N2RiZGU1NmY5NDA5NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KMJVXb5n0eWifW6HiJn67amyw2H9xN15dcv6g3LIxus',
-        'base_url' => 'https://api.themoviedb.org/3',
-        'img_url'  => 'https://image.tmdb.org/t/p/w500'
+        'api_key' => $_ENV["API_KEY"],
+        'token' => $_ENV["TOKEN_TMDB"],
+        'base_url' => $_ENV["BASE_URL"],
+        'img_url'  => $_ENV["IMG_URL"]
     ],
     'rutas' =>  [],
+    // Sitio web
     'site' => [
-        'name' => 'Eurofilm',
-        'url'  => 'http://localhost/Eurofilm/public'
+        'name' => $_ENV["SITE"],
+        'url'  => $_ENV["URL"] ?? 'http://localhost/Eurofilm/'
     ]
 ];
