0d2be776bd6a6f71298b9a221dd1a85ba17cfe2d
mikelnaval2006@gmail.com <mikelnaval2006@gmail.com>
2026-06-15 17:01:18 +0200

    logica de la aplicacion

--- public/js/busqueda.js
+++ public/js/busqueda.js
@@ -1,13 +1,11 @@
 import { apiGet } from "./api.js";
+import { BASE_URL } from "./configuracion.js";
 import { renderList } from "./ui.js";
-
 
 // Referencias
 const btnBuscar = document.getElementById("btn-buscar");
 const inputBusqueda = document.getElementById("busqueda-titulo");
 const container = document.getElementById("peliculas");
-
-
 
 // Input busqueda (evento Input)
 inputBusqueda.addEventListener("input", async () => {
@@ -15,18 +13,17 @@ inputBusqueda.addEventListener("input", async () => {
   const query = inputBusqueda.value.trim();
   console.log("QUERY:", query);
 
-  
   // Si el input está vacío, limpiamos el contenedor y salimos
   if (query === "") {
-    container.innerHTML = ""; 
+    container.innerHTML = "";
     window.location.reload();
     return;
   }
   try {
     const movies = await apiGet("/search/movie", { query: query });
-    renderList(movies,container);
+    renderList(movies, container);
     console.log("IS ARRAY:", Array.isArray(movies));
-    console.log("MOVIES:",movies);
+    console.log("MOVIES:", movies);
   } catch (error) {
     console.error("Error en la búsqueda:", error);
   }
@@ -36,11 +33,16 @@ btnBuscar.addEventListener("click", async () => {
   console.log("QUERY:", query);
 
   if (query === "") return;
-  const movies = await apiGet("/search/movie", { query: query });
-  console.log("MOVIES API:", movies);
-
-
-  
-  renderList(movies,container);
+  try {
+    const url = `${BASE_URL}/api/search/movie?query=${encodeURIComponent(query)}`;
+    const res = await fetch(url);
+    // validacion de respuesta del servidor
+    if (!res.ok) {
+      throw new Error(`Error HTTP: ${res.status}`);
+    }
+    const data = await res.json();
+    renderList(data, container);
+  } catch (error) {
+    console.error(error);
+  }
 });
-
